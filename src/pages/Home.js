b07afb34f1626f90b27758ca64477003bcdd017e
vrishaan-taneja-2025 <iconicovsavarjoy@gmail.com>
2025-08-27 19:41:01 +0530

    Add src/pages/Home.js 1756303860781

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="overlay">
+            <img src="./Img/logo.svg" alt="Club Logo" className="logo" />
+            <h1>Autonomous Initiative Club</h1>
+            <p>Create. Automate. Innovate.</p>
+        </div>
+    );
+};
+
+export default Home;
